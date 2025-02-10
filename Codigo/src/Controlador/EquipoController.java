96525b65b201195e61daa2c1a2315dea4dffed1a
Koldo <koldo.soriguren@ikasle.egibide.org>
2025-02-10 10:47:28 +0100

    Main funcional
    En cada clase esta el constructor añadido

--- Codigo/src/Controlador/EquipoController.java
+++ Codigo/src/Controlador/EquipoController.java
@@ -1,4 +1,12 @@
 package Controlador;
 
+import Modelo.EquipoDAO;
+import Modelo.JugadorDAO;
+
 public class EquipoController {
+    private EquipoDAO eqiopoDAO;
+
+    public EquipoController(EquipoDAO eqiopoDAO) {
+        this.eqiopoDAO = eqiopoDAO;
+    }
 }
