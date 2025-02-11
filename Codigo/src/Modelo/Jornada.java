c9c09d96d695135808753b886b394281ed4d5bd3
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-02-11 10:01:28 +0100

    Añadir constructor a Jornada

--- Codigo/src/Modelo/Jornada.java
+++ Codigo/src/Modelo/Jornada.java
@@ -1,37 +1,46 @@
 package Modelo;
 
-import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Jornada {
-    private String cod_jornada;
+    private String codJornada;
     private String resultado;
-    private Date fecha_jornada;
+    private Date fechaJornada;
     private Competicion competicion;
+    private List<Enfrentamiento> enfrentamientos;
 
     public Jornada() {
     }
 
     public Jornada(String cod_jornada, Date fecha_jornada, String resultado) {
-        this.cod_jornada = cod_jornada;
-        this.fecha_jornada = fecha_jornada;
+        this.codJornada = cod_jornada;
+        this.fechaJornada = fecha_jornada;
         this.resultado = resultado;
     }
 
+    public Jornada(String codJornada, LocalDate fecha) {
+        this.codJornada = codJornada;
+        this.fechaJornada = fechaJornada;
+        this.enfrentamientos = new ArrayList<>();
+    }
+
     public String getCod_jornada() {
-        return cod_jornada;
+        return codJornada;
     }
 
     public void setCod_jornada(String cod_jornada) {
-        this.cod_jornada = cod_jornada;
+        this.codJornada = cod_jornada;
     }
 
     public Date getFecha_jornada() {
-        return fecha_jornada;
+        return fechaJornada;
     }
 
     public void setFecha_jornada(Date fecha_jornada) {
-        this.fecha_jornada = fecha_jornada;
+        this.fechaJornada = fecha_jornada;
     }
 
     public String getResultado() {
@@ -40,5 +49,13 @@ public class Jornada {
 
     public void setResultado(String resultado) {
         this.resultado = resultado;
+    }
+
+    public String mostrarJornada() {
+        StringBuilder sb = new StringBuilder("Jornada ").append(codJornada).append(" - Fecha: ").append(fechaJornada).append("\n");
+        for (Enfrentamiento e : enfrentamientos) {
+            sb.append("- ").append(e).append("\n");
+        }
+        return sb.toString();
     }
 }
