ae1ed9e09eb9698868de5138d311572f44f4e738
Koldo <koldo.soriguren@ikasle.egibide.org>
2025-02-11 08:46:25 +0100

    Clase enfrentamiento
    a

--- Codigo/src/Modelo/Enfrentamiento.java
+++ Codigo/src/Modelo/Enfrentamiento.java
@@ -1,5 +1,42 @@
 package Modelo;
 
-public class Enfrentamiento {
+import java.time.LocalTime;
 
+public class Enfrentamiento {
+    private String cod_emfrentamiento;
+    private String string;
+    private LocalTime hora;
+
+    public Enfrentamiento() {
+    }
+
+    public Enfrentamiento(String cod_emfrentamiento, LocalTime hora, String string) {
+        this.cod_emfrentamiento = cod_emfrentamiento;
+        this.hora = hora;
+        this.string = string;
+    }
+
+    public String getCod_emfrentamiento() {
+        return cod_emfrentamiento;
+    }
+
+    public void setCod_emfrentamiento(String cod_emfrentamiento) {
+        this.cod_emfrentamiento = cod_emfrentamiento;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
 }
