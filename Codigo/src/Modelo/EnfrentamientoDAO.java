ac1ca956c922b2c42bec0472f94300a4174bac2a
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-02-11 09:55:53 +0100

    Pequeños cambios en diferentes clases

--- Codigo/src/Modelo/EnfrentamientoDAO.java
+++ Codigo/src/Modelo/EnfrentamientoDAO.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class EnfrentamientoDAO {
     private List<Enfrentamiento> enfrentamientos;
+
     public EnfrentamientoDAO() {
         enfrentamientos = new ArrayList<Enfrentamiento>();
     }
+
     public void guardarEnfrentamientos(Enfrentamiento e){
         enfrentamientos.add(e);
     }
+
     public List<Enfrentamiento> getEnfrentamientos() {
         return enfrentamientos;
     }
--- Codigo/src/Modelo/EquipoDAO.java
+++ Codigo/src/Modelo/EquipoDAO.java
@@ -1,4 +1,11 @@
 package Modelo;
 
+import java.util.List;
+
 public class EquipoDAO {
+    private List<Equipo> equipos;
+
+    public List<Equipo> obtenerEquipos() {
+        return equipos;
+    }
 }
