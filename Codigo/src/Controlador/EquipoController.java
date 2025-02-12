433ba61e720cbc4a9bd4a4774481d590b9e5f185
Bouya-Ahmed <bouyaahmed.chrifbouya@ikasle.egibide.org>
2025-02-12 09:25:27 +0100

    Solo he puesto algunas expresiones regulares.

--- Codigo/src/Controlador/EquipoController.java
+++ Codigo/src/Controlador/EquipoController.java
@@ -20,12 +20,46 @@ public class EquipoController {
 
     public void altaValidarDatosEquipo() {
         String cod = solicitarDatos("Código", "Introduce el código del equipo", "^[0-9]{4}$");
-        String nombre = solicitarDatos("Nombre", "Introduce el nombre del equipo", "^[A-Z][a-z]*$");
-        LocalDate fecha = formatearFecha(solicitarDatos("Fecha de fundación", "Introduce la fecha de fundación del equipo", ""));
+        String nombre = solicitarDatos("Nombre", "Introduce el nombre del equipo", "^[A-Z][a-z]+(?:\\s[A-Z][a-z]+)*$");
+        LocalDate fecha = formatearFecha(solicitarDatos("Fecha de fundación", "Introduce la fecha de fundación del equipo", "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$\n"));
 
         Equipo equipo = new Equipo(cod, nombre, fecha);
 
         equipoDAO.insertar(equipo);
+    }
+
+    public void modificar() {
+        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
+
+        Equipo[] opciones = equipos.toArray(new Equipo[0]);
+        Equipo opcionElegida = (Equipo) JOptionPane.showInputDialog(null, "Elija a que equipo le quiere modificar los datos", "Modificación", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        opcionElegida.setNombreEquipo(solicitarDatos("Nombre", "Introduce el nuevo nombre del equipo", "^[A-Z][a-z]+(?:\\s[A-Z][a-z]+)*$"));
+        opcionElegida.setFechaFund(formatearFecha(solicitarDatos("Fecha de fundación", "Introduce la nueva fecha de fundación del equipo", "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$")));
+
+        JOptionPane.showMessageDialog(null, "Se han modificado los datos del equipo correctamente");
+    }
+
+    public void borrar() {
+        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
+
+        Equipo[] opciones = equipos.toArray(new Equipo[0]);
+        Equipo opcionElegida = (Equipo) JOptionPane.showInputDialog(null, "Elige a que equipo le quieres dar de baja", "Dar de Baja", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+        equipoDAO.borrar(opcionElegida);
+
+        JOptionPane.showMessageDialog(null, "Se ha dado de baja al equipo con éxito", "Baja Completada", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void mostrar() {
+        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
+
+        JOptionPane.showMessageDialog(null, equipos);
+    }
+
+    private LocalDate formatearFecha(String fecha) {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(fecha, formato);
     }
 
     private String solicitarDatos(String dato, String mensaje, String exprRegular) {
@@ -54,41 +88,5 @@ public class EquipoController {
         } while (terminar);
 
         return variable;
-    }
-
-    public void modificar() {
-
-        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
-
-        Equipo[] opciones = equipos.toArray(new Equipo[0]);
-        Equipo opcionElegida = (Equipo) JOptionPane.showInputDialog(null, "Elija a que equipo le quiere modificar los datos", "Modificación", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-
-        opcionElegida.setNombreEquipo(solicitarDatos("Nombre", "Introduce el nuevo nombre del equipo", "^[A-Z][a-z]+(?:\\s[A-Z][a-z]+)*$"));
-        opcionElegida.setFechaFund(formatearFecha(solicitarDatos("Fecha de fundación", "Introduce la nueva fecha de fundación del equipo", "^$")));
-
-        JOptionPane.showMessageDialog(null, "Se han modificado los datos del equipo correctamente");
-    }
-
-    public void borrar() {
-
-        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
-
-        Equipo[] opciones = equipos.toArray(new Equipo[0]);
-        Equipo opcionElegida = (Equipo) JOptionPane.showInputDialog(null, "Elige a que equipo le quieres dar de baja", "Dar de Baja", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-
-        equipoDAO.borrar(opcionElegida);
-
-        JOptionPane.showMessageDialog(null, "Se ha dado de baja al equipo con éxito", "Baja Completada", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    public void mostrar() {
-        ArrayList<Equipo> equipos = equipoDAO.getEquipos();
-
-        JOptionPane.showMessageDialog(null, equipos);
-    }
-
-    private LocalDate formatearFecha(String fecha) {
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return LocalDate.parse(fecha, formato);
     }
 }
